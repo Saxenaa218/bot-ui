987a31aba36f3d3b66b67250539ed82d07a5e4c0
Abhishek Saxena <abhisheks@netlink.com>
2020-01-28 15:41:30 +0530

    skeleton added

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.scss';
+import { Button, Popover } from 'antd';
+import 'antd/dist/antd.css';
+
+class App extends React.Component {
+  state = {
+    title: "demo",
+    list: [{ msg: "demo", by: "me", t:null }]
+  };
+
+  render() {
+
+    const component = (
+      <div>
+        {this.state.list.map(e => (
+          <div></div>
+        ))}
+      </div>
+    );
+
+    return (
+      <div className="index-component">
+        <Popover
+          content={component}
+          title={this.state.title}
+          trigger="click"
+          placement="topRight"
+        >
+          <Button icon="msg" />
+        </Popover>
+      </div>
+    );
+  }
+}
+
+export default App;
