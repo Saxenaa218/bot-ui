5e27af3aa9cc3754c4cb2cfe0509a2dd183ec3e2
Abhishek Saxena <abhisheks@netlink.com>
2020-01-30 18:44:51 +0530

    changes

--- apis/server.js
+++ apis/server.js
@@ -1,48 +1,31 @@
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-// var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-// router.post('/', function(req, res) {
-//   res.send(res);
-// });
+var port = process.env.PORT || 8080;
 
 app.route('/book')
   .get(function (req, res) {
-    res.send('Get a random book')
+    res.send('Get a random book');
   })
   .post(function (req, res) {
     // console.log(req.body);
-    res.send(req.body)
+    res.send(req.body);
   })
   .put(function (req, res) {
     res.send('Update the book')
   })
 
-app.route('')
+// app.route('')
 
-// router.post('/post', function (req, res) {
-//   res.json({ message: 'POST request to the homepage' });
-// })
+// var birds = require('./birds')
+// app.use('/birds', birds)
 
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-// app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
 app.listen(port);
 console.log('listening on ' + port);
